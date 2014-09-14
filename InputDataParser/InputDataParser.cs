00fe45f0123036ef3e1e3097fbf9e8a1e7b17b17
Alexander Kamyshnikov <axill777@gmail.com>
2014-09-14 18:35:10 +0400

    Initial demo CLI parser and data

---
+++ InputDataParser/InputDataParser.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using System.Xml.Schema;
+using System.Xml.Serialization;
+
+namespace InputDataParser
+{
+    using PeopleMap = Dictionary<int, List<PeopleTypes.TMan>>;
+    using ApertureMap = Dictionary<int, List<ApertureTypes.TAperture>>;
+    using FurnitureMap = Dictionary<int, List<FurnitureTypes.TFurnitureItem>>;
+    using GeometryMap = Dictionary<int, List<GeometryTypes.TGeometryItem>>;
+
+    public class Parser
+    {
+        /*public void ValidateXML(string schemaFileName, string xmlFileName)
+        {
+            var schemas = new XmlSchemaSet();
+            schemas.Add("", schemaFileName);
+
+            Console.WriteLine("Attempting to validate {0}", xmlFileName);
+            var xmlDoc = XDocument.Load(xmlFileName);
+            bool errors = false;
+            xmlDoc.Validate(schemas, (o, e) =>
+            {
+                Console.WriteLine("{0}", e.Message);
+                errors = true;
+            });
+            Console.WriteLine("{0}: {1}", xmlFileName, errors ? "did not validate" : "validated");
+            Console.WriteLine();
+
+            IEnumerable<XElement> people =
+                from seg in xmlDoc.Descendants()
+                where (string)seg.Element("ClassName") == "TMan"
+                select seg;
+
+            int c = people.Count(); // 46 + 92 = 138
+            foreach( var el in people)
+            {
+                Console.WriteLine( el.Element("Color").Value );
+            }
+
+            Console.WriteLine();
+        }*/
+
+        public PeopleMap LoadPeopleXML( string fileName )
+        {
+            var result = new PeopleMap();
+            var serializer = new XmlSerializer(typeof(PeopleTypes.TBuilding));
+            var reader = new FileStream(fileName, FileMode.Open);
+            var building = serializer.Deserialize(reader) as PeopleTypes.TBuilding;
+            foreach (var floor in building.FloorList)
+            {
+                var peopleList = new List<PeopleTypes.TMan>();
+                foreach (var man in floor.People)
+                {
+                    peopleList.Add(man);
+                }
+
+                result.Add(floor.Number, peopleList);
+            }
+            return result;
+        }
+
+        public ApertureMap LoadApertureXML(string fileName)
+        {
+            var result = new ApertureMap();
+            var serializer = new XmlSerializer(typeof(ApertureTypes.TBuilding));
+            var reader = new FileStream(fileName, FileMode.Open);
+            var building = serializer.Deserialize(reader) as ApertureTypes.TBuilding;
+            foreach (var floor in building.FloorList)
+            {
+                var apertureList = new List<ApertureTypes.TAperture>();
+                foreach (var aperture in floor.ApertureList)
+                {
+                    apertureList.Add(aperture);
+                }
+
+                result.Add(floor.Number, apertureList);
+            }
+            return result;
+        }
+
+        public GeometryMap LoadGeometryXML(string fileName)
+        {
+            var result = new GeometryMap();
+            var serializer = new XmlSerializer(typeof(GeometryTypes.TBuilding));
+            var reader = new FileStream(fileName, FileMode.Open);
+            var building = serializer.Deserialize(reader) as GeometryTypes.TBuilding;
+            foreach (var floor in building.FloorList)
+            {
+                var geometryList = new List<GeometryTypes.TGeometryItem>();
+                foreach (var room in floor.RoomList)
+                {
+                    foreach(var geometryItem in room.Geometry)
+                        geometryList.Add(geometryItem);
+                }
+
+                result.Add(floor.Number, geometryList);
+            }
+            return result;
+        }
+    }
+}
---
+++ ParserTest/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParserTest
+{
+    using PeopleMap = Dictionary<int, List<PeopleTypes.TMan>>;
+    using ApertureMap = Dictionary<int, List<ApertureTypes.TAperture>>;
+    using FurnitureMap = Dictionary<int, List<FurnitureTypes.TFurnitureItem>>;
+    using GeometryMap = Dictionary<int, List<GeometryTypes.TGeometryItem>>;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            InputDataParser.Parser inputParser = new InputDataParser.Parser();
+  //          peopleParser.ValidateXML(@"..\..\KinderGarten\садик17_people.xsd", @"..\..\KinderGarten\садик17_people.xml");
+            /*PeopleMap people = inputParser.LoadPeopleXML(@"..\..\KinderGarten\садик17_people.xml");
+            List<int> ids = new List<int>();
+            foreach (var manPair in people)
+            {
+                Console.WriteLine("Floor number: " + manPair.Key);
+                Console.WriteLine("-------------------------------");
+                foreach( var man in manPair.Value )
+                {
+                    Console.WriteLine("Man Id: " + man.Id);
+                    if (ids.Contains(man.Id))
+                        Console.WriteLine("!!!!");
+                    else
+                        ids.Add(man.Id);
+                }
+            }*/
+
+            /*ApertureMap apertures = inputParser.LoadApertureXML(@"..\..\KinderGarten\садик17_door.xml");
+            List<int> ids = new List<int>();
+            foreach (var aperturePair in apertures)
+            {
+                Console.WriteLine("Floor number: " + aperturePair.Key);
+                Console.WriteLine("-------------------------------");
+                foreach (var aperture in aperturePair.Value)
+                {
+                    Console.WriteLine("Aperture Id: " + aperture.Id);
+                    if (ids.Contains(aperture.Id))
+                        Console.WriteLine("!!!!");
+                    else
+                        ids.Add(aperture.Id);
+                }
+            }*/
+
+            /*FurnitureMap furniture = inputParser.LoadFurnitureXML(@"..\..\KinderGarten\садик17_furniture.xml");
+            foreach (var furniturePair in furniture)
+            {
+                Console.WriteLine("Floor number: " + furniturePair.Key);
+                Console.WriteLine("-------------------------------");
+                foreach (var furnitureItem in furniturePair.Value)
+                {
+                    Console.WriteLine("FurnitureItem Id: " + furnitureItem.Id);
+                }
+            }*/
+
+            GeometryMap geometry = inputParser.LoadGeometryXML(@"..\..\KinderGarten\садик17_geometry.xml");
+            foreach (var geometryPair in geometry)
+            {
+                Console.WriteLine("Floor number: " + geometryPair.Key);
+                Console.WriteLine("-------------------------------");
+                foreach (var geometryItem in geometryPair.Value)
+                {
+                    Console.WriteLine("GeometryItem Id: " + geometryItem.Id);
+                }
+            }
+
+            Console.ReadKey(false);
+        }
+    }
+}
