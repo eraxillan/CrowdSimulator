b87884c0a3cb9bba8831bf3f5b53747e845c95bd
Alexander Kamyshnikov <axill777@gmail.com>
2015-01-06 13:06:58 +0300

    GdiPlusVisualizator: fix box text drawing

---
+++ CommonLibrary/DrawingUtils.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonLibrary
+{
+    public class DrawingUtils
+    {
+        public static void DrawText( Graphics g, string text, PointF ptStart, RectangleF extent )
+        {
+            var gs = g.Save();
+            // Inverse Y axis again - now it grow down;
+            // if we don't do this, text will be drawn inverted
+            g.ScaleTransform( 1.0f, -1.0f, MatrixOrder.Prepend );
+
+            // Find the maximum appropriate text size to fix the extent
+            float fontSize = 100.0f;
+            Font fnt = null;
+            RectangleF textRect;
+            SizeF textSize;
+            do
+            {
+                fnt = new Font( "Arial", fontSize / g.DpiX, FontStyle.Bold, GraphicsUnit.Pixel );
+                textSize = g.MeasureString( text, fnt );
+                textRect = new RectangleF( new PointF( ptStart.X - textSize.Width / 2.0f, -ptStart.Y - textSize.Height / 2.0f ), textSize );
+
+                var textRectInv = new RectangleF( textRect.X, -textRect.Y, textRect.Width, textRect.Height );
+                if ( extent.Contains( textRectInv ) )
+                    break;
+
+                fontSize -= 1.0f;
+                if ( fontSize <= 0 )
+                {
+                    fontSize = 1.0f;
+                    break;
+                }
+            } while ( true );
+
+            // Create a StringFormat object with the each line of text, and the block of text centered on the page
+            var stringFormat = new StringFormat()
+            {
+                Alignment = StringAlignment.Center,
+                LineAlignment = StringAlignment.Center
+            };
+            g.DrawString( text, fnt, Brushes.Black, textRect, stringFormat );
+            stringFormat.Dispose();
+
+            g.Restore( gs );
+        }
+    }
+}
--- InputDataParser/WrapperTypes.cs
+++ InputDataParser/WrapperTypes.cs
@@ -633,8 +633,6 @@ namespace SigmaDC.Types
     public class BoxWrapper : GeometryItemWrapper, IVisualisable
     {
         GeometryTypes.TBox m_box = null;
-        Font m_textFont = null;
-        RectangleF m_textRect;
 
         public BoxWrapper( GeometryTypes.TBox box )
             : base( box )
@@ -660,6 +658,16 @@ namespace SigmaDC.Types
             return new RectangleF( x1, y1, w, h );
         }
 
+        public float Z1
+        {
+            get { return m_box.Z1; }
+        }
+
+        public float Z2
+        {
+            get { return m_box.Z2; }
+        }
+
         public bool LimePen
         {
             get; set;
@@ -683,60 +691,10 @@ namespace SigmaDC.Types
                     {
                         g.DrawRectangle( brownPen, extent.X, extent.Y, extent.Width, extent.Height );
                     }
-
-                    // Draw box properties as text on it
-                    var extentCenter = new PointF( ( extent.Left + extent.Right ) / 2, ( extent.Bottom + extent.Top ) / 2 );
-                    DrawText( g, "ID: " + m_box.Id + "\n" + "Height: " + ( m_box.Z2 - m_box.Z1 ), extentCenter, extent );
                     break;
                 }
                 default: throw new InvalidOperationException( "Invalid TBox type" );
             }
-        }
-
-        private void DrawText( Graphics g, string text, PointF ptStart, RectangleF extent )
-        {
-            var gs = g.Save();
-            // Inverse Y axis again - now it grow down;
-            // if we don't do this, text will be drawn inverted
-            g.ScaleTransform( 1.0f, -1.0f, MatrixOrder.Prepend );
-
-            if ( m_textFont == null )
-            {
-                // Find the maximum appropriate text size to fix the extent
-                float fontSize = 100.0f;
-                Font fnt;
-                SizeF textSize;
-                do
-                {
-                    fnt = new Font( "Arial", fontSize / g.DpiX, FontStyle.Bold, GraphicsUnit.Pixel );
-                    textSize = g.MeasureString( text, fnt );
-                    m_textRect = new RectangleF( new PointF( ptStart.X - textSize.Width / 2.0f, -ptStart.Y - textSize.Height / 2.0f ), textSize );
-
-                    var textRectInv = new RectangleF( m_textRect.X, -m_textRect.Y, m_textRect.Width, m_textRect.Height );
-                    if ( extent.Contains( textRectInv ) )
-                        break;
-
-                    fontSize -= 1.0f;
-                    if ( fontSize <= 0 )
-                    {
-                        fontSize = 1.0f;
-                        break;
-                    }
-                } while ( true );
-
-                m_textFont = fnt;
-            }
-
-            // Create a StringFormat object with the each line of text, and the block of text centered on the page
-            var stringFormat = new StringFormat()
-            {
-                Alignment = StringAlignment.Center,
-                LineAlignment = StringAlignment.Center
-            };
-            g.DrawString( text, m_textFont, Brushes.Black, m_textRect, stringFormat );
-            stringFormat.Dispose();
-
-            g.Restore( gs );
         }
     }
 
@@ -987,6 +945,16 @@ namespace SigmaDC.Types
                     {
                         foreach ( var human in m_people ) human.Draw( g );
                     }
+
+                    // Draw box subscriptions
+                    foreach ( var box in Geometry )
+                    {
+                        // Draw box properties as text over it
+                        var extent = box.Extents;
+                        var extentCenter = new PointF( ( extent.Left + extent.Right ) / 2, ( extent.Bottom + extent.Top ) / 2 );
+                        CommonLibrary.DrawingUtils.DrawText( g, "ID: " + box.Id + "\n" + "Height: " + ( box.Z2 - box.Z1 ), extentCenter, extent );
+                    }
+
                     break;
                 }
                 default: throw new InvalidOperationException( "Invalid TFloor type" );
